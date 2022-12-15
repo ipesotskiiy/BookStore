2275183a76c3c80e9bf0c4c09eff9ecb330ba57c
Ireal-ai <pesotskygoscha@yandex.ru>
2022-12-15 18:38:20 +0300

    created serializers

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -36,15 +36,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user',
-    'product',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'user',
+    'product'
 ]
 
 MIDDLEWARE = [
---
+++ product/serializer.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from product.models import Comment, Genre, Rating, Book
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('date', 'text')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name',)
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('name',)
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            'comment',
+            'genre',
+            'rating',
+            'title',
+            'author',
+            'price',
+            'cover',
+            'date_of_issue',
+            'in_stock',
+            'description',
+            'average_rate',
+            'is_in_favorite'
+        )
