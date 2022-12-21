4c45ba7ee794a6729c0849219020cdddbcf872a3
Ireal-ai <pesotskygoscha@yandex.ru>
2022-12-21 17:34:58 +0300

    started update user model

--- user/models.py
+++ user/models.py
@@ -8,17 +8,12 @@ from product.models import Rating, Comment, Book
 class User(models.Model):
     ratings = models.ForeignKey(Rating, on_delete=models.CASCADE)
     comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    favorites = models.ForeignKey(Book, on_delete=models.CASCADE)
+    favorites = models.ManyToManyField(Book)
     role = models.CharField(verbose_name='User role', max_length=10)
     email = models.EmailField(verbose_name='Your email', max_length=40, db_index=True)
     name = models.CharField(verbose_name='Your name', max_length=30)
     password = models.CharField(verbose_name='Your password', max_length=255)
-    avatar = models.ImageField(
-        verbose_name='avatar',
-        null=True,
-        blank=True,
-        default='images/anonim_user.jpg'
-    )
+    avatar = models.CharField(verbose_name='Your avatar', max_length=250, null=True, blank=True)
 
     class Meta:
         verbose_name = 'User'
