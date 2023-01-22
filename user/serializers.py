fd18f15bf626e648d29e2e15444a9e55ec2a1d7b
Игорь Песоцкий <pesotskygoscha@yandex.ru>
2023-01-22 19:56:58 +0300

    Delete serializers.py

--- user/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from user.models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('ratings', 'comments', 'favorites', 'role', 'email', 'name', 'password', 'avatar')
