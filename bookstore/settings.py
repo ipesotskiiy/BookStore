417e5a5e45b1ccd0ec17a3ffe266eacf06e472b6
Ireal-ai <pesotskygoscha@yandex.ru>
2023-02-25 19:59:00 +0300

    update settings and user urls[C

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -147,16 +147,6 @@ SWAGGER_SETTINGS = {
 }
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
 
--- user/urls.py
+++ user/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('auth/signup', RegisterUserAPIView.as_view(), name='signup'),
     path('auth/signin', MyTokenObtainPairView.as_view(), name='signin'),
     path('auth/check-token', views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/user/me', GetProfileView.as_view(), name='me'),
-    path('api/user/upload-avatar', UploadAvatarView.as_view(), name='upload_avatar'),
-    path('api/user/<pk>', UpdateUserView.as_view(), name='update_user')
+    path('user/me', GetProfileView.as_view(), name='me'),
+    path('user/upload-avatar', UploadAvatarView.as_view(), name='upload_avatar'),
+    path('user/<pk>', UpdateUserView.as_view(), name='update_user')
 ]
