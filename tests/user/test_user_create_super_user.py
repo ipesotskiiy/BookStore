68aac91750ea127cfc318615838e37c572edc2d1
Ireal-ai <pesotskygoscha@yandex.ru>
2023-08-29 21:20:58 +0300

    feat: test

--- tests/user/test_user_create_super_user.py
+++ tests/user/test_user_create_super_user.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.django_db
-def test_create_super_user(create_super_user):
-    user = create_super_user
-    print(user.pk)
+# @pytest.mark.django_db
+# def test_create_super_user(create_super_user):
+#     user = create_super_user
+#     print(user.pk)
